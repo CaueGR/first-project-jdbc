4d6ac3ca90b1659ac271207c6ac6210af25015a6
Caue Robattini <cauegr18@gmail.com>
2025-03-27 12:29:44 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoImlJDBC;
+
 public class DaoFactory {
 
+	public static SellerDao createSellerDao() {
+		return new SellerDaoImlJDBC(DB.getConnection());
+	}
 }
