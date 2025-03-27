e25a5d98bb384d0c32d1202bff754a1dd5c7a62a
Caue Robattini <cauegr18@gmail.com>
2025-03-27 13:40:41 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,5 +28,13 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert ====");
+		//instanciando novo seller:
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		//inserir no banco de dados:
+		sellerDao.insert(newSeller);
+		System.out.println("Inseted! New id = " + newSeller.getId());
+		
 	}
 }
